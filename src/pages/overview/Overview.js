df0baa0388f15e8df4b8ebc0daaa5cb03bc6e3aa
Tutifruiti <Tutifruiti@users.noreply.github.com>
2020-10-20 14:43:24 +0200

    Card test

--- src/pages/overview/Overview.js
+++ src/pages/overview/Overview.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import Stocks from './components/stocks'
 
 export default function Overview() {
-  return <h1>Overview</h1>;
+  return(
+  <div>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6}>
+        <Stocks/>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Stocks/>
+      </Grid>
+    </Grid>
+  </div>
+  )
 }
