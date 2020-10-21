3bed19b78b68289da13c5ec2117362fb05d3e085
Tutifruiti <Tutifruiti@users.noreply.github.com>
2020-10-21 16:54:05 +0200

    Updates

---
+++ precache-manifest.c4a380497a23ad6ef38ba1104d92c53a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555fad2ed06544081e54f9429d9acdc0",
+    "url": "/react_frontend/index.html"
+  },
+  {
+    "revision": "c3a001b4078946936656",
+    "url": "/react_frontend/static/js/2.0a2eef95.chunk.js"
+  },
+  {
+    "revision": "99929baffdc57587c92fa16b4d95c618",
+    "url": "/react_frontend/static/js/2.0a2eef95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0f3a8a79a78b9e7e5c",
+    "url": "/react_frontend/static/js/main.47ff5c99.chunk.js"
+  },
+  {
+    "revision": "0abab8eebc202668a1b9",
+    "url": "/react_frontend/static/js/runtime-main.99f01956.js"
+  },
+  {
+    "revision": "3d432ca2badb7e0130b379f9162b99b1",
+    "url": "/react_frontend/static/media/logo.3d432ca2.svg"
+  }
+]);
