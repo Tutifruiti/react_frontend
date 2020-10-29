0e72448ae0c04f168f4724cba95e42ad86a953ed
Tutifruiti <Tutifruiti@users.noreply.github.com>
2020-10-29 18:21:58 +0100

    Updates

---
+++ precache-manifest.07b0f040bcadd5ef1a35824ec6023e7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495ee38d4a4fdf2eff2d8accb65283d2",
+    "url": "/react_frontend/index.html"
+  },
+  {
+    "revision": "f24e24ea20459e31cdaf",
+    "url": "/react_frontend/static/js/2.90f4834f.chunk.js"
+  },
+  {
+    "revision": "1ce680386e740132dfd0b72a65c78577",
+    "url": "/react_frontend/static/js/2.90f4834f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6033a6119036371c0b9",
+    "url": "/react_frontend/static/js/main.aec42fa4.chunk.js"
+  },
+  {
+    "revision": "0abab8eebc202668a1b9",
+    "url": "/react_frontend/static/js/runtime-main.99f01956.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_frontend/precache-manifest.98dc4bff097b81813f86c2230e2a1973.js"
+  "/react_frontend/precache-manifest.07b0f040bcadd5ef1a35824ec6023e7f.js"
 );
 
 self.addEventListener('message', (event) => {
